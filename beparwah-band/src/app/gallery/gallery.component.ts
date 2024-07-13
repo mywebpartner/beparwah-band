fe63f3af01e7614168964ddb70757689f0076eb4
Aditya <f20220497@goa.bits-pilani.ac.in>
2024-07-13 23:21:01 +0530

    added photos

--- beparwah-band/src/app/gallery/gallery.component.ts
+++ beparwah-band/src/app/gallery/gallery.component.ts
@@ -15,13 +15,7 @@ export class GalleryComponent implements OnInit {
     'assets/image6.jpg',
     'assets/image7.jpg',
     'assets/image8.jpg',
-    'assets/image9.jpg',
-    'assets/image10.jpg',
-    'assets/image11.jpg',
-    'assets/image12.jpg',
-    'assets/image13.jpg',
-    'assets/image14.jpg',
-    'assets/image15.jpg'
+    'assets/image9.jpg'
   ];
 
   currentIndex = 0;
--- beparwah-band/src/app/meet-the-band/meet-the-band.component.ts
+++ beparwah-band/src/app/meet-the-band/meet-the-band.component.ts
@@ -10,19 +10,19 @@ export class MeetTheBandComponent implements OnInit {
     {
       name: 'SAHIL KAMBOJ',
       role: 'SINGER',
-      image: 'assets/john-doe.jpg',
+      image: 'assets/singer.png',
       description: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque harum voluptates veniam amet aut esse omnis accusantium distinctio quisquam assumenda accusamus nobis labore sunt tempora aliquam, quasi nam rerum perferendis impedit? Sint quisquam quae ex beatae repellat dicta iste neque odit nobis itaque? Facere, laborum consequuntur voluptatem odio sint rem.'
     },
     {
       name: 'ARGAD',
       role: 'PIANIST',
-      image: 'assets/jane-smith.jpg',
+      image: 'assets/pianist.png',
       description: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque harum voluptates veniam amet aut esse omnis accusantium distinctio quisquam assumenda accusamus nobis labore sunt tempora aliquam, quasi nam rerum perferendis impedit? Sint quisquam quae ex beatae repellat dicta iste neque odit nobis itaque? Facere, laborum consequuntur voluptatem odio sint rem.'
     },
     {
       name: 'AYUSH KASHYAP',
       role: 'PERCUSSIONS',
-      image: 'assets/bob-johnson.jpg',
+      image: 'assets/pess.png',
       description: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque harum voluptates veniam amet aut esse omnis accusantium distinctio quisquam assumenda accusamus nobis labore sunt tempora aliquam, quasi nam rerum perferendis impedit? Sint quisquam quae ex beatae repellat dicta iste neque odit nobis itaque? Facere, laborum consequuntur voluptatem odio sint rem.'
     }
   ];
